968c25233a50d3041b430389f0cc499f41c85a93
anton-arangelov <arangelov.a@abv.bg>
2022-12-20 17:54:15 +0200

    Cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3001',
-    credentials: true,
+    origin: 'localhost:3001',
   });
   await app.listen(3000);
 }
