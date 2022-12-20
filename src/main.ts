c60cb2ac362cf004321a6636a1659f36b13a4f99
anton-arangelov <arangelov.a@abv.bg>
2022-12-20 19:32:50 +0200

    Cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use({
-    cors: {
-      credentials: true,
-      origin: 'http://localhost:3000',
-    },
-  });
+
   await app.listen(3000);
 }
 bootstrap();
