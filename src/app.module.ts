cfcc173856eca95b047c525c7b518d0a315da753
anton-arangelov <arangelov.a@abv.bg>
2022-12-20 14:57:30 +0200

    Cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,10 @@ import { ChessModule } from './chess/chess.module';
       playground: false,
       autoSchemaFile: true,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      cors: {
+        origin: 'http://localhost:3001',
+        credentials: true,
+      },
     }),
     ChessModule,
   ],
