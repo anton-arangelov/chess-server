bff259b10b7365e713e39231081d1b6cb1fe5cd1
anton-arangelov <arangelov.a@abv.bg>
2022-12-20 19:03:17 +0200

    Cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: 'localhost:3001',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
